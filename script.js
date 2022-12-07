ebe0c857354206a8231ec26a327290eb6389a2d8
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2022-12-07 22:44:13 +0100

    updated

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 let themes = document.getElementsByClassName('theme')
 
-console.log('hey');
+let theme = localStorage.getItem('theme')
+
+if (theme == null) {
+    setTheme('blue')
+} else {
+    setTheme(theme)
+}
 
 for (let i=0; i < themes.length; i++) {
     themes[i].addEventListener('click', function() {
         let mode = this.dataset.mode;
-        console.log('clicked', mode)
         setTheme(mode)
     })   
 }
 
-const setTheme = (mode) => {
+function setTheme(mode) {
     if (mode === "light") {
         document.getElementById('theme-styles').href="./styles/index.css"
     }
@@ -26,4 +31,10 @@ const setTheme = (mode) => {
     if (mode === "blue") {
         document.getElementById('theme-styles').href="./styles/blue.css"
     }
+
+    localStorage.setItem('theme', mode)
 }
+
+const copyright = document.getElementById('copyright')
+let year = new Date().getFullYear();
+copyright.textContent = `copyright © ${year}`
